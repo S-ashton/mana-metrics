0c66ce9a1bba4ee74379c60368c85baf344da6c4
S-ashton <sophieannashton@icloud.com>
2026-08-20 13:53:08 +0100

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>ManaMetrics</h1>
+      <p>Commander analytics, without the spreadsheet.</p>
+    </div>
+  );
+}
+
+export default App;
